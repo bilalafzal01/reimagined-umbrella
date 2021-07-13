2bdbccc86715eccc4c82084e14345f2917016790
Bilal Afzal <bilal@shiftcrowd.co.uk>
2021-07-13 19:11:59 +0500

    removed api+app

--- packages/api/src/modules/shared/middleware/UseCacheControl.ts
+++
@@ -1,9 +0,0 @@
-import { CacheHint } from "apollo-cache-control"
-import { UseMiddleware } from "type-graphql"
-
-export function UseCacheControl(hint: CacheHint) {
-  return UseMiddleware(({ info }, next) => {
-    info.cacheControl.setCacheHint(hint)
-    return next()
-  })
-}
